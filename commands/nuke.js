293a70bc8e124508ca000d1a5882c6715ebdaed4
selbstmord <67484724+selbstmord@users.noreply.github.com>
2021-02-25 09:03:58 +0100

    Update nuke.js
    Updated nuke command to sync permissions

--- commands/nuke.js
+++ commands/nuke.js
@@ -1,26 +1,19 @@
-const Discord = require("discord.js");
-const { set } = require("quick.db");
-
 module.exports.help = {
   name: "nuke",
 };
 
 module.exports.run = async (client, message, args) => {
-  if (!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) return message.reply("you don't have the permission for that.");
-  if (!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.channel.send("I don't have the permission for that.");
+  if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("you don't have the permission for that.");
+  if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("I don't have the permission for that.");
 
-  const chanName = message.channel.name;
-  const position = message.channel.position;
-  const categoryId = message.channel.parentID;
-  const catego = message.guild.channels.cache.find((c) => c.id == categoryId && c.type == "category");
-  message.channel.delete().catch();
-
-  const chan = message.guild.channels
-    .create(chanName, {
-      type: "text",
-      parent: catego,
-    })
-    .then((channel) => channel.setPosition(position))
-    .then((channel) => channel.send(`Succesfully nuked ${chanName} \n  https://i.gifer.com/6Ip.gif`).then((msg) => msg.delete({ timeout: 20000 })));
-  chan;
+  if (!message.channel.parentID) {
+    message.channel.clone({ position: message.channel.rawPosition }).then((ch) => {
+      ch.send("Channel nuked \n https://i.gifer.com/6Ip.gif");
+    });
+  } else {
+    message.channel.clone({ parent: message.channel.parentID, position: message.channel.rawPosition }).then((ch) => {
+      ch.send("Channel nuked \n https://i.gifer.com/6Ip.gif");
+    });
+  }
+  message.channel.delete();
 };
